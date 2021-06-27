5e21ee52bfc4ae6914adb5c9a878ddaab5faa5e6
barshein <82324960+barshein@users.noreply.github.com>
2021-06-27 15:07:46 +0300

    Update Model.kt

--- app/src/main/java/com/example/remotecontroljoystick/Model.kt
+++ app/src/main/java/com/example/remotecontroljoystick/Model.kt
@@ -1,25 +1,17 @@
 package com.example.remotecontroljoystick
 
 import java.io.PrintWriter
-import java.lang.Thread.sleep
 import java.net.Socket
-import java.util.*
 import java.util.concurrent.BlockingQueue
-import java.util.concurrent.Executors
 import java.util.concurrent.LinkedBlockingQueue
 
 object Model {
 
-    //    val e = LiveData<Double>
-//    val a = LiveData<Double>
-//    val t = LiveData<Double>
-//    val r = LiveData<Double>}
     val q: BlockingQueue<String> = LinkedBlockingQueue<String>() //create queue
 
     // connect to flightgear
     fun connectServer(ip: String, port: Int) {
-        //val socket = Socket(ip, port)
-        val socket = Socket("192.168.0.191", 6565)
+        val socket = Socket(ip, port)
         val out = PrintWriter(socket.outputStream, true)
 
         while (true) {
